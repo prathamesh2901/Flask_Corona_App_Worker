baf952a80d9fce1dcb1fe00fc0af7d98f7981afe
Prathamesh Sawant <prathamesh2901@gmail.com>
2020-05-28 21:41:59 -0700

    Added Parameters

--- receive.py
+++ receive.py
@@ -3,7 +3,15 @@ import pika
 import pymysql
 import json
 import concurrent.futures
+from os import environ
 
+DB_URL = environ["DB_URL"]
+DB_USER = environ["DB_USER"]
+DB_PASSWORD = environ["DB_PASSWORD"]
+DB_NAME = environ["DB_NAME"]
+MQ_USER = environ["MQ_USER"]
+MQ_PASSWORD = environ["MQ_PASSWORD"]
+MQ_URL = environ["MQ_URL"]
 
 def callback(ch, method, properties, body):
     print(method.routing_key)
@@ -13,21 +21,21 @@ def callback(ch, method, properties, body):
     data = json.loads(body)
     print(data)
     conn = pymysql.connect(
-      user="pratham2901",
-      password="Corona99",
-      host="db",
+      user=DB_USER,
+      password=DB_PASSWORD,
+      host=DB_URL,
       port=3306)
     cur = conn.cursor()
 
-    query = "REPLACE INTO corona.{0}(name, cases, deaths, recoveries, date) VALUES (%s, %s, %s, %s, %s)".format(method.routing_key)
+    query = "REPLACE INTO {0}.{1}(name, cases, deaths, recoveries, date) VALUES (%s, %s, %s, %s, %s)".format(DB_NAME,method.routing_key)
 
     cur.execute(query,( data["name"],data["cases"],data["deaths"],data["recoveries"],data["date"]))
     conn.commit()
     conn.close()
 
 def proc(que):
-    credentials = pika.PlainCredentials('pratham2901', 'Corona99')
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',credentials=credentials))
+    credentials = pika.PlainCredentials(MQ_USER, MQ_PASSWORD)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(MQ_URL, credentials=credentials))
     channel = connection.channel()
     channel.queue_declare(queue=que,durable=True)
 
@@ -44,4 +52,3 @@ with concurrent.futures.ProcessPoolExecutor() as executor:
     ques = ['countries','states']
     for que in ques:
         executor.submit(proc, que)
-
